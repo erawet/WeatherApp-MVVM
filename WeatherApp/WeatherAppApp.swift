71c65b8c2c6d523a6b1302cb9b8d7c5b75e09260
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2026-06-27 22:18:39 -0500

    Initial project setup

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Don Wettasinghe on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
